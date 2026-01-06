c0dd81e68a7503c81745ca8acbb912e4d077cd01
muhyidin-svg <60212542+muhyidin-svg@users.noreply.github.com>
2026-01-07 00:25:37 +0700

    Add mufrodat data

---
+++ Hayya/js/data.js
@@ -0,0 +1,30 @@
+const mufrodat = [
+  {
+    id: "profesi01",
+    arab: "شُرْطِيٌّ",
+    arti: "Polisi",
+    image: "assets/images/profesi/profesi01.jpg",
+    audio: "assets/audio/profesi/profesi01.mp3"
+  },
+  {
+    id: "profesi02",
+    arab: "جُنْدِيٌّ",
+    arti: "Tentara",
+    image: "assets/images/profesi/profesi02.jpg",
+    audio: "assets/audio/profesi/profesi02.mp3"
+  },
+  {
+    id: "profesi03",
+    arab: "مُدَرِّسٌ",
+    arti: "Guru",
+    image: "assets/images/profesi/profesi03.jpg",
+    audio: "assets/audio/profesi/profesi03.mp3"
+  },
+  {
+    id: "profesi04",
+    arab: "طَبِيبٌ",
+    arti: "Dokter",
+    image: "assets/images/profesi/profesi04.jpg",
+    audio: "assets/audio/profesi/profesi04.mp3"
+  }
+];
